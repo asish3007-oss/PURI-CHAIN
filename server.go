b50e9696136b3e180b1d0e0546292c129caa05d9
asish3007-oss <asish3007@gmail.com>
2025-11-29 10:30:45 +0530

    Update server.go

--- server.go
+++ server.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
-	"log"
-	"net/http"
+"encoding/json"
+"io"
+"log"
+"net/http"
 )
 
 type MineRequest struct {
-	Data       string json:"data"
-	Difficulty int    json:"difficulty"
+Data string `json:"data"`
+Difficulty int `json:"difficulty"`
 }
 
 func handleMine(w http.ResponseWriter, r *http.Request) {
-	body, _ := io.ReadAll(r.Body)
-	var req MineRequest
-	json.Unmarshal(body, &req)
+body, _ := io.ReadAll(r.Body)
+var req MineRequest
+json.Unmarshal(body, &req)
 
-	addBlock(req.Data, req.Difficulty)
+addBlock(req.Data, req.Difficulty)
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Blockchain)
+w.Header().Set("Content-Type", "application/json")
+json.NewEncoder(w).Encode(Blockchain)
 }
 
 func handleChain(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Blockchain)
+w.Header().Set("Content-Type", "application/json")
+json.NewEncoder(w).Encode(Blockchain)
 }
 
 func startServer() {
-	http.HandleFunc("/mine", handleMine)
-	http.HandleFunc("/chain", handleChain)
+http.HandleFunc("/mine", handleMine)
+http.HandleFunc("/chain", handleChain)
 
-	log.Println("🔥 PURI-CHAIN node running on http://localhost:8080")
+log.Println("🔥 PURI-CHAIN node running on http://localhost:8080")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+log.Fatal(http.ListenAndServe(":8080", nil))
 }
